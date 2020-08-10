aac195a07abd3ef27121a37dc6643606fdbf9057
qk w <846718424@qq.com>
2020-08-10 21:20:01 +0800

    添加前端页面和视图

--- TT/settings.py
+++ TT/settings.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'TT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "static")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
